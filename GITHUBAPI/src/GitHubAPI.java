61c90f1ff783592ba0732a57fb06c18f95534a4b
corryco <55395145+corryco@users.noreply.github.com>
2019-11-04 21:24:06 +0000

    Version 0.1 GITHUBAPI

---
+++ GITHUBAPI/src/GitHubAPI.java
@@ -0,0 +1,32 @@
+
+import java.io.IOException;
+
+import java.text.MessageFormat;
+
+
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+public class GitHubAPI 
+{
+
+	
+	ublic static void main(String[] args) throws IOException {
+
+		final String user = "defunkt";
+
+		final String format = "{0}) {1}- created on {2}";
+
+		int count = 1;
+
+		RepositoryService service = new RepositoryService();
+
+		for (Repository repo : service.getRepositories(user))
+
+			System.out.println(MessageFormat.format(format, count++,
+
+					repo.getName(), repo.getCreatedAt()));
+
+	}
+}
