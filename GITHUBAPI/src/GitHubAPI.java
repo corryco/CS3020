b35c7591a253ebc527a8d3e02d23c18e61521f86
corryco <55395145+corryco@users.noreply.github.com>
2019-11-04 22:53:53 +0000

    version 0.4 Core Respository Connect

--- GITHUBAPI/src/GitHubAPI.java
+++ GITHUBAPI/src/GitHubAPI.java
@@ -1,29 +1,28 @@
 
 import java.io.IOException;
-
 import java.text.MessageFormat;
 
-
-
-import org.elipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.service.RepositoryService;
 
 public class GitHubAPI 
 {
-
-	
 	public static void main(String[] args) throws IOException 
 	{
 
 		final String user = "defunkt";
+		final String name = "defunkt";
 
 		final String format = "{0}) {1}- created on {2}";
 
 		int count = 1;
 
 		RepositoryService service = new RepositoryService();
-
-		for(Repository repo : service.getRepositories(user))
+		
+		service.getRepository(user, name);
+		
+//		for(Repository repo : service.getRepositories(user))
+		for(Repository repo : service.getRepositories(user, name))
 		{
 			System.out.println(MessageFormat.format(format, count++, repo.getName(), repo.getCreatedAt()));
 		}
