a04b5614d84be5486cf5cb4bcc149a137f790631
corryco <55395145+corryco@users.noreply.github.com>
2019-11-05 01:43:45 +0000

    version 0.6 Junit tests

--- GITHUBAPI/src/GitHubAPI.java
+++ GITHUBAPI/src/GitHubAPI.java
@@ -1,30 +1,86 @@
 
 import java.io.IOException;
-import java.text.MessageFormat;
+import java.net.URL; 
 
+import org.eclipse.egit.github.core.Repository; 
+import org.eclipse.egit.github.core.RepositoryId; 
+import org.eclipse.egit.github.core.User; 
+import org.eclipse.egit.github.core.service.RepositoryService; 
+import org.eclipse.egit.github.core.client.GitHubClient; 
+
+import org.eclipse.egit.github.core.Comment;
+import org.eclipse.egit.github.core.Gist;
+import org.eclipse.egit.github.core.GistFile;
+import org.eclipse.egit.github.core.IRepositoryIdProvider;
+import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.Label;
+import org.eclipse.egit.github.core.Milestone;
 import org.eclipse.egit.github.core.Repository;
-import org.eclipse.egit.github.core.service.RepositoryService;
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.RepositoryContents;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.client.GitHubRequest;
+import org.eclipse.egit.github.core.client.GitHubResponse;
+import org.eclipse.egit.github.core.client.PageIterator;
+import org.eclipse.egit.github.core.client.PagedRequest;
+import org.eclipse.egit.github.core.service.CollaboratorService;
+import org.eclipse.egit.github.core.service.CommitService;
+import org.eclipse.egit.github.core.service.ContentsService;
+import org.eclipse.egit.github.core.service.GistService;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.LabelService;
+import org.eclipse.egit.github.core.service.MarkdownService;
+import org.eclipse.egit.github.core.service.MilestoneService;
+import org.eclipse.egit.github.core.service.UserService;
 
 public class GitHubAPI 
 {
+	
 	public static void main(String[] args) throws IOException 
 	{
-
-		final String user = "defunkt";
-		final String name = "defunkt";
-
-		final String format = "{0}) {1}- created on {2}";
-
-		int count = 1;
-
-		RepositoryService service = new RepositoryService();
 		
-		service.getRepository(user, name);
+		String username = "corryco";
+		String password ="Uaz9401CC";
 		
-//		for(Repository repo : service.getRepositories(user))
-		for(Repository repo : service.getRepositories(user, name))
-		{
-			System.out.println(MessageFormat.format(format, count++, repo.getName(), repo.getCreatedAt()));
-		}
+		GitHubClient client = null; 
+
+		client = new GitHubClient();
+		client.setCredentials(username, password);
+		
+		RepositoryService repoService  = new RepositoryService(client); 
+		UserService serService = new UserService(client);
+		IssueService issueService = new IssueService(client);
+		MilestoneService milestoneService = new MilestoneService(client);
+		LabelService labelService = new LabelService(client);
+		CommitService commitService = new CommitService(client);
+		MarkdownService markdownService = new MarkdownService(client);
+		CollaboratorService colaboratorService = new CollaboratorService(client);
+		ContentsService contentsService = new ContentsService(client);
+		
+		/*		
+		GitHubClient client = null; 
+		//String url = ("github.test.url");
+		String url = "https://github.com/corryco/CS3020.git";
+
+		if (url != null) 
+		{ 
+				URL parsed = new URL(url); 
+				client = new GitHubClient(parsed.getHost(), parsed.getPort(), 
+				parsed.getProtocol()); 
+		} 
+		else 
+			client = new GitHubClient(); 
+		
+		String user;
+		String password;
+		String writableRepo;
+		 	 
+		 user = System.getProperty("github.test.user"); 
+		 password = System.getProperty("github.test.password"); 
+		 writableRepo = System.getProperty("github.test.repository"); 
+
+		 client.setCredentials(user, password);
+*/
 	}
 }
--- GITHUBAPI/src/TESTGITHUBAPI.java
+++ GITHUBAPI/src/TESTGITHUBAPI.java
@@ -2,13 +2,118 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.IOException; 
+import java.util.List; 
+import java.util.Map; 
+import java.util.Map.Entry; 
+ 
+import org.eclipse.egit.github.core.Repository; 
+import org.eclipse.egit.github.core.RepositoryId; 
+import org.eclipse.egit.github.core.User; 
+import org.eclipse.egit.github.core.service.RepositoryService; 
+import org.eclipse.egit.github.core.client.GitHubClient; 
+
+import org.junit.Ignore; 
+
+
+
+///http://www.javased.com/index.php?source_dir=egit-github/org.eclipse.egit.github.core.tests/src/org/eclipse/egit/github/core/tests/live/LiveTest.java
+
+
+
 public class TESTGITHUBAPI 
 {
 
+	GitHubClient client; 
+	
 	@Test
 	public void test() 
 	{
 		fail("Not yet implemented");
 	}
 
+	@Test 
+	 @Ignore 
+	 public void createRepository() throws IOException 
+	{ 
+	  assertNotNull("Client user is required", client.getUser()); 
+	  RepositoryService service = new RepositoryService(client); 
+	  Repository repository = new Repository(); 
+	  repository.setOwner(new User().setLogin(client.getUser())); 
+	  repository.setName("test-create-" + System.currentTimeMillis()); 
+	  repository.setPrivate(true); 
+	  Repository created = service.createRepository(repository); 
+	  assertNotNull(created); 
+	  assertNotSame(repository, created); 
+	  assertTrue(created.isPrivate()); 
+	  assertEquals(repository.getOwner(), created.getOwner()); 
+	  assertEquals(repository.getName(), created.getName()); 
+	 } 
+	
+	@Test 
+	 public void fetchRepositories() throws IOException { 
+	  RepositoryService service = new RepositoryService(client); 
+	  List<Repository> repos = service.getRepositories("defunkt"); 
+	  assertNotNull(repos); 
+	  assertFalse(repos.isEmpty()); 
+	  for (Repository repo : repos) { 
+	   assertNotNull(repo); 
+	   assertNotNull(repo.getName()); 
+	   assertNotNull(repo.getOwner()); 
+	   assertNotNull(repo.getUrl()); 
+	   assertNotNull(repo.getCreatedAt()); 
+	   assertTrue(repo.getSize() >= 0); 
+	   assertTrue(repo.getForks() >= 0); 
+	   assertTrue(repo.getOpenIssues() >= 0); 
+	   assertTrue(repo.getWatchers() >= 0); 
+	  } 
+	 } 
+	
+	@Test 
+	public void getLanguages() throws IOException 
+	{
+		  RepositoryService service=new RepositoryService(client);
+		  Map<String,Long> languages=service.getLanguages(new RepositoryId("defunkt","resque"));
+		  assertNotNull(languages);
+		  assertFalse(languages.isEmpty());
+		  for ( Entry<String, Long> language : languages.entrySet()) {
+		    assertNotNull(language.getKey());
+		    assertFalse(language.getKey().length() == 0);
+		    assertTrue(language.getValue() > 0);
+		  }
+	}
+	
+	/**
+	  * Test forking a repository 
+	  * 
+	  * @throws Exception 
+	  */ 
+	 @Test 
+	 @Ignore 
+	 public void forkRepository() throws Exception { 
+	  assertNotNull("Client user is required", client.getUser()); 
+	  RepositoryService service = new RepositoryService(client); 
+	  service.forkRepository(new RepositoryId(client.getUser(), "resque")); 
+	 } 
+	 
+	 /**
+	  * Test fetching forks of a repository 
+	  * 
+	  * @throws IOException 
+	  */ 
+	 @Test 
+	 public void fetchForks() throws IOException 
+	 { 
+		  RepositoryService service = new RepositoryService(client); 
+		  List<Repository> repos = service.getForks(new RepositoryId("defunkt", 
+		    "resque")); 
+		  assertNotNull(repos); 
+		  assertFalse(repos.isEmpty()); 
+		  for (Repository repo : repos) 
+		  { 
+		   assertNotNull(repo); 
+		   assertTrue(repo.isFork()); 
+		  } 
+	 } 
+	 
 }
