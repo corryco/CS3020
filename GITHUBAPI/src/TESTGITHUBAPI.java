091fac28ed96144a335dac5b7574021874a8d599
corryco <55395145+corryco@users.noreply.github.com>
2019-11-06 00:02:06 +0000

    Version 0.10 Additional junit test

--- GITHUBAPI/src/TESTGITHUBAPI.java
+++ GITHUBAPI/src/TESTGITHUBAPI.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 import java.io.IOException; 
@@ -16,6 +17,8 @@ import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.Commit; 
 import org.eclipse.egit.github.core.CommitUser; 
 import org.eclipse.egit.github.core.Tree; 
+import org.eclipse.egit.github.core.service.UserService; 
+import org.eclipse.egit.github.core.User; 
 
 import org.junit.Ignore; 
 
@@ -24,133 +27,160 @@ import org.junit.Ignore;
 public class TESTGITHUBAPI 
 {
 
-	GitHubClient client; 
+	protected GitHubClient client = null; 
+
 	
-	@Test
-	public void test() 
+	public void createclientuser()
 	{
-		fail("Not yet implemented");
+		String username = "corryco";
+		String password ="Uaz9401CC";
+		client = new GitHubClient();
+		client.setCredentials(username, password);
+	}
+						
+	public void createclienttoken()
+	{
+		//OAuth2 token authentication
+		GitHubClient client = new GitHubClient();
+		client.setOAuth2Token("75add7dbe95ddac306e707ff9d3ed30ba5768612");
 	}
 
 	@Test 
-	 @Ignore 
-	 public void createRepository() throws IOException 
+	public void fetchCurrentUserUserPWD() throws Exception 
 	{ 
-	  assertNotNull("Client user is required", client.getUser()); 
-	  RepositoryService service = new RepositoryService(client); 
-	  Repository repository = new Repository(); 
-	  repository.setOwner(new User().setLogin(client.getUser())); 
-	  repository.setName("test-create-" + System.currentTimeMillis()); 
-	  repository.setPrivate(true); 
-	  Repository created = service.createRepository(repository); 
-	  assertNotNull(created); 
-	  assertNotSame(repository, created); 
-	  assertTrue(created.isPrivate()); 
-	  assertEquals(repository.getOwner(), created.getOwner()); 
-	  assertEquals(repository.getName(), created.getName()); 
-	 } 
-	
-	@Test 
-	 public void fetchRepositories() throws IOException { 
-	  RepositoryService service = new RepositoryService(client); 
-	  List<Repository> repos = service.getRepositories("defunkt"); 
-	  assertNotNull(repos); 
-	  assertFalse(repos.isEmpty()); 
-	  for (Repository repo : repos) { 
-	   assertNotNull(repo); 
-	   assertNotNull(repo.getName()); 
-	   assertNotNull(repo.getOwner()); 
-	   assertNotNull(repo.getUrl()); 
-	   assertNotNull(repo.getCreatedAt()); 
-	   assertTrue(repo.getSize() >= 0); 
-	   assertTrue(repo.getForks() >= 0); 
-	   assertTrue(repo.getOpenIssues() >= 0); 
-	   assertTrue(repo.getWatchers() >= 0); 
-	  } 
-	 } 
-	
-	@Test 
-	public void getLanguages() throws IOException 
-	{
-		  RepositoryService service=new RepositoryService(client);
-		  Map<String,Long> languages=service.getLanguages(new RepositoryId("defunkt","resque"));
-		  assertNotNull(languages);
-		  assertFalse(languages.isEmpty());
-		  for ( Entry<String, Long> language : languages.entrySet()) {
-		    assertNotNull(language.getKey());
-		    assertFalse(language.getKey().length() == 0);
-		    assertTrue(language.getValue() > 0);
-		  }
-	}
-	
-	/**
-	  * Test forking a repository 
-	  * 
-	  * @throws Exception 
-	  */ 
-	 @Test 
-	 @Ignore 
-	 public void forkRepository() throws Exception { 
-	  assertNotNull("Client user is required", client.getUser()); 
-	  RepositoryService service = new RepositoryService(client); 
-	  service.forkRepository(new RepositoryId(client.getUser(), "resque")); 
+		createclientuser();
+		assertNotNull("Test requires user", client.getUser()); 
+
+		UserService service = new UserService(client); 
+		User user = service.getUser(); 
+		assertNotNull(user); 
+		assertEquals(client.getUser(), user.getLogin()); 
+		assertNotNull(user.getGravatarId()); 
+		assertNotNull(user.getAvatarUrl()); 
+		assertNotNull(user.getCreatedAt()); 
+		assertNotNull(user.getPlan()); 
 	 } 
 	 
-	 /**
-	  * Test fetching forks of a repository 
-	  * 
-	  * @throws IOException 
-	  */ 
+	@Test 
+	public void fetchCurrentUserToken() throws Exception 
+	{ 
+	
+		createclienttoken();
+		assertNotNull("Test requires user", client.getUser()); 
+	 
+		UserService service = new UserService(client); 
+		User user = service.getUser(); 
+		assertNotNull(user); 
+		assertEquals(client.getUser(), user.getLogin()); 
+		assertNotNull(user.getGravatarId()); 
+		assertNotNull(user.getAvatarUrl()); 
+		assertNotNull(user.getCreatedAt()); 
+		assertNotNull(user.getPlan()); 
+	 } 
+
+	 @Test 
+	 public void createRepository() throws IOException 
+	{ 
+		 createclientuser();
+		 assertNotNull("Client user is required", client.getUser()); 
+
+		 RepositoryService service = new RepositoryService(client); 
+		 Repository repository = new Repository(); 
+		 repository.setOwner(new User().setLogin(client.getUser())); 
+		 repository.setName("test-create-" + System.currentTimeMillis()); 
+		 repository.setPrivate(true); 
+		 
+		 Repository created = service.createRepository(repository); 
+		 assertNotNull(created); 
+		 assertNotSame(repository, created); 
+		 assertTrue(created.isPrivate()); 
+		 assertEquals(repository.getOwner(), created.getOwner()); 
+		 assertEquals(repository.getName(), created.getName()); 
+	 } 
+	
+	 @Test 
+	 public void fetchRepositories() throws IOException 
+	 { 
+		 createclientuser();
+		 assertNotNull("Client user is required", client.getUser()); 
+
+		 RepositoryService service = new RepositoryService(client); 
+		 List<Repository> repos = service.getRepositories("defunkt"); 
+		 assertNotNull(repos); 
+		 assertFalse(repos.isEmpty()); 
+
+		 for (Repository repo : repos) 
+		 { 
+			 assertNotNull(repo); 
+			 assertNotNull(repo.getName()); 
+			 assertNotNull(repo.getOwner()); 
+			 assertNotNull(repo.getUrl()); 
+			 assertNotNull(repo.getCreatedAt()); 
+			 assertTrue(repo.getSize() >= 0); 
+			 assertTrue(repo.getForks() >= 0); 
+			 assertTrue(repo.getOpenIssues() >= 0); 
+			 assertTrue(repo.getWatchers() >= 0); 
+		} 
+	} 
+	
+	@Test 
+	public void forkRepository() throws Exception
+	{ 
+		createclientuser();
+		assertNotNull("Client user is required", client.getUser()); 
+		
+		RepositoryService service = new RepositoryService(client); 
+		service.forkRepository(new RepositoryId(client.getUser(), "resque")); 
+	 } 
+	 
 	 @Test 
 	 public void fetchForks() throws IOException 
 	 { 
-		  RepositoryService service = new RepositoryService(client); 
-		  List<Repository> repos = service.getForks(new RepositoryId("defunkt", 
-		    "resque")); 
-		  assertNotNull(repos); 
-		  assertFalse(repos.isEmpty()); 
-		  for (Repository repo : repos) 
-		  { 
-		   assertNotNull(repo); 
-		   assertTrue(repo.isFork()); 
-		  } 
-	 } 
-	 /**
-	  * Test default state of commit 
-	  */ 
-	 @Test 
-	 public void defaultState() 
-	 { 
-		Commit commit = new Commit(); 
-		assertNull(commit.getAuthor()); 
-		assertNull(commit.getCommitter()); 
-		assertNull(commit.getMessage()); 
-		assertNull(commit.getParents()); 
-		assertNull(commit.getSha()); 
-		assertNull(commit.getTree()); 
-		assertNull(commit.getUrl()); 
-		assertEquals(0, commit.getCommentCount()); 
-	 } 
+		 createclientuser();
+		 assertNotNull("Client user is required", client.getUser()); 
+		 
+		 RepositoryService service = new RepositoryService(client); 
+		 List<Repository> repos = service.getForks(new RepositoryId("defunkt", "resque")); 
+		 assertNotNull(repos); 
+		 assertFalse(repos.isEmpty()); 
+		 
+		 for (Repository repo : repos) 
+		 { 
+			 assertNotNull(repo); 
+			 assertTrue(repo.isFork()); 
+		 } 
+	 }
 	 
-	 /**
-	  * Test updating commit fields 
-	  */ 
 	 @Test 
-	 public void updateFields() 
+	 public void commitdefaultState() 
 	 { 
-		Commit commit = new Commit(); 
-		CommitUser author = new CommitUser().setName("Art Thor"); 
-		assertEquals(author, commit.setAuthor(author).getAuthor()); 
-		CommitUser committer = new CommitUser().setName("Comb Mitter"); 
-		assertEquals(committer, commit.setCommitter(committer).getCommitter()); 
-		assertEquals("commit message", commit.setMessage("commit message").getMessage()); 
-		assertEquals(new ArrayList<Commit>(), commit.setParents(new ArrayList<Commit>()).getParents()); 
-		assertEquals("abcdef", commit.setSha("abcdef").getSha()); 
+		 Commit commit = new Commit(); 
+		 assertNull(commit.getAuthor()); 
+		 assertNull(commit.getCommitter()); 
+		 assertNull(commit.getMessage()); 
+		 assertNull(commit.getParents()); 
+		 assertNull(commit.getSha()); 
+		 assertNull(commit.getTree()); 
+		 assertNull(commit.getUrl()); 
+		 assertEquals(0, commit.getCommentCount()); 
+	 } 
+
+	 @Test 
+	 public void commitupdateFields()
+	 { 
+		 Commit commit = new Commit(); 
+		 CommitUser author = new CommitUser().setName("Art Thor"); 
+		 assertEquals(author, commit.setAuthor(author).getAuthor()); 
+		 CommitUser committer = new CommitUser().setName("Comb Mitter"); 
+		 assertEquals(committer, commit.setCommitter(committer).getCommitter()); 
+		 assertEquals("commit message", commit.setMessage("commit message").getMessage()); 
+		 assertEquals(new ArrayList<Commit>(), commit.setParents(new ArrayList<Commit>()).getParents()); 
+		 assertEquals("abcdef", commit.setSha("abcdef").getSha()); 
 		
-		Tree tree = new Tree(); 
-		tree.setSha("12345"); 
-		assertEquals(tree, commit.setTree(tree).getTree()); 
-		assertEquals("url", commit.setUrl("url").getUrl()); 
-		assertEquals(32, commit.setCommentCount(32).getCommentCount()); 
-	 } 	 
+		 Tree tree = new Tree(); 
+		 tree.setSha("12345"); 
+		 assertEquals(tree, commit.setTree(tree).getTree()); 
+		 assertEquals("url", commit.setUrl("url").getUrl()); 
+		 assertEquals(32, commit.setCommentCount(32).getCommentCount()); 
+	 }
 }
