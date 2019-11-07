0543713331266de16283028e721954a145035003
corryco <55395145+corryco@users.noreply.github.com>
2019-11-07 00:00:38 +0000

    version 0.14 Adding Unit test for Watchers

--- GITHUBAPI/src/TESTGITHUBAPI.java
+++ GITHUBAPI/src/TESTGITHUBAPI.java
@@ -1,12 +1,9 @@
 import static org.junit.Assert.*;
-
-
 import org.junit.Test;
+import org.junit.Before; 
 
 import java.io.IOException; 
 import java.util.List; 
-//import java.util.Map; 
-//import java.util.Map.Entry; 
 import java.util.ArrayList; 
 
 import org.eclipse.egit.github.core.Repository; 
@@ -18,35 +15,24 @@ import org.eclipse.egit.github.core.Commit;
 import org.eclipse.egit.github.core.CommitUser; 
 import org.eclipse.egit.github.core.Tree; 
 import org.eclipse.egit.github.core.service.UserService; 
-import org.eclipse.egit.github.core.User; 
-
-//import org.junit.Ignore; 
+import org.eclipse.egit.github.core.service.WatcherService; 
 
 
-//https://stackoverflow.com/questions/46215024/adding-file-to-github-using-java-client-org-eclipse-egit-github-core
-	
 public class TESTGITHUBAPI 
 {
 	protected GitHubClient client = null; 
 
-	public void createclientuser(String username, String password)
+	@Before
+	public void createclientuser()
 	{
 		client = new GitHubClient();
-		client.setCredentials(username, password);
-	}
-						
-	public void createclienttoken(String strToken )
-	{
-		//OAuth2 token authentication
-		client = new GitHubClient();
-		client.setOAuth2Token(strToken);
+		client.setCredentials("corryco", "Uaz9401CC");
 	}
 
 	@Test 
 	public void fetchCurrentUserUserPWD() throws Exception 
 	{ 
 		client = null;
-		createclientuser("corryco", "Uaz9401CC");
 		assertNotNull("Test requires user", client.getUser()); 
 
 		UserService service = new UserService(client); 
@@ -62,9 +48,10 @@ public class TESTGITHUBAPI
 	@Test 
 	public void fetchCurrentUserToken() throws Exception 
 	{ 
-		client = null;
-		createclienttoken("09224f92fb573e6eb6e3f878710b235c9030115d");
-		UserService service = new UserService(client); 
+		GitHubClient tokclient = new GitHubClient();
+		tokclient.setOAuth2Token("09224f92fb573e6eb6e3f878710b235c9030115d");
+
+		UserService service = new UserService(tokclient); 
 		User user = service.getUser(); 
 		assertNotNull(user); 
 		assertNotNull(user.getGravatarId()); 
@@ -76,7 +63,6 @@ public class TESTGITHUBAPI
 	@Test 
 	 public void createRepository() throws IOException 
 	{ 
-		 createclientuser("corryco", "Uaz9401CC");
 		 assertNotNull("Client user is required", client.getUser()); 
 
 		 RepositoryService service = new RepositoryService(client); 
@@ -95,11 +81,10 @@ public class TESTGITHUBAPI
 	 @Test 
 	 public void fetchRepositories() throws IOException 
 	 { 
-		 createclientuser("corryco", "Uaz9401CC");
 		 assertNotNull("Client user is required", client.getUser()); 
 
 		 RepositoryService service = new RepositoryService(client); 
-		 List<Repository> repos = service.getRepositories("defunkt"); 
+		 List<Repository> repos = service.getRepositories("corryco"); 
 		 assertNotNull(repos); 
 		 assertFalse(repos.isEmpty()); 
 
@@ -118,9 +103,74 @@ public class TESTGITHUBAPI
 	} 
 	
 	 @Test 
+	  //Test fetching followers 
+	 public void fetchFollowers() throws Exception 
+	 { 
+		assertNotNull("Test requires user", client.getUser()); 
+
+		UserService service = new UserService(client); 
+		List<User> users = service.getFollowers(); 
+		assertNotNull(users); 
+		for (User user : users) 
+		{ 
+			assertNotNull(user.getId()); 
+			assertNotNull(user.getLogin()); 
+		} 
+	 } 
+	 
+	 @Test 
+	  //Test fetching followed users 
+	 public void fetchFollowing() throws Exception 
+	 { 
+		assertNotNull("Test requires user", client.getUser()); 
+	  
+		assertNotNull("Test requires user", client.getUser()); 
+		UserService service = new UserService(client); 
+		List<User> users = service.getFollowing(); 
+		assertNotNull(users); 
+		for (User user : users) 
+		{ 
+			assertNotNull(user.getId()); 
+			assertNotNull(user.getLogin()); 
+		} 
+	 } 
+	 
+	 @Test 
+	 //Test getting watchers of a repository 
+	 public void getWatchers() throws Exception 
+	 { 
+		 assertNotNull("Client user is required", client.getUser()); 
+
+		 WatcherService service = new WatcherService(client); 
+		 List<User> watchers = service.getWatchers(new RepositoryId("defunkt", "dotjs")); 
+		 assertNotNull(watchers); 
+		 for (User watcher : watchers) 
+		 { 
+			 assertNotNull(watcher); 
+			 assertNotNull(watcher.getLogin()); 
+		 } 
+	 } 
+	 
+	 @Test 
+	  //Test getting repositories watched by a user 
+	 public void getWatched() throws Exception 
+	 { 
+		 assertNotNull("Client user is required", client.getUser()); 
+
+		 WatcherService service = new WatcherService(client); 
+		  List<Repository> watched = service.getWatched("corryco"); 
+		  assertNotNull(watched); 
+		  for (Repository repo : watched) 
+		  { 
+			   assertNotNull(repo); 
+			   assertNotNull(repo.getName()); 
+			   assertNotNull(repo.getOwner()); 
+		  } 
+	 } 
+	 
+	 @Test 
 	 public void fetchForks() throws IOException 
 	 { 
-		 createclientuser("corryco", "Uaz9401CC");
 		 assertNotNull("Client user is required", client.getUser()); 
 		 
 		 RepositoryService service = new RepositoryService(client); 
@@ -153,16 +203,16 @@ public class TESTGITHUBAPI
 	 public void commitupdateFields()
 	 { 
 		 Commit commit = new Commit(); 
-		 CommitUser author = new CommitUser().setName("Art Thor"); 
+		 CommitUser author = new CommitUser().setName("Conor Corry"); 
 		 assertEquals(author, commit.setAuthor(author).getAuthor()); 
-		 CommitUser committer = new CommitUser().setName("Comb Mitter"); 
+		 CommitUser committer = new CommitUser().setName("Joe Blogs"); 
 		 assertEquals(committer, commit.setCommitter(committer).getCommitter()); 
-		 assertEquals("commit message", commit.setMessage("commit message").getMessage()); 
+		 assertEquals("Another commit message", commit.setMessage("Another commit message").getMessage()); 
 		 assertEquals(new ArrayList<Commit>(), commit.setParents(new ArrayList<Commit>()).getParents()); 
-		 assertEquals("abcdef", commit.setSha("abcdef").getSha()); 
+		 assertEquals("TestText", commit.setSha("TestText").getSha()); 
 		
 		 Tree tree = new Tree(); 
-		 tree.setSha("12345"); 
+		 tree.setSha("NewTestText"); 
 		 assertEquals(tree, commit.setTree(tree).getTree()); 
 		 assertEquals("url", commit.setUrl("url").getUrl()); 
 		 assertEquals(32, commit.setCommentCount(32).getCommentCount()); 
